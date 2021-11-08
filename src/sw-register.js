86a6c446c44d61f0a7452408c6e1783840d7f7b7
Zhenhua Che <zhenhua.che@shopee.com>
2021-11-08 18:57:38 +0800

    fix: 解除生产环境的限制

--- src/sw-register.js
+++ src/sw-register.js
@@ -1,7 +1,6 @@
 export default function LocalServiceWorkerRegister() {
-  console.log(process.env, 1111)
   const swPath = `${process.env.PUBLIC_URL}/sw.js`;
-  if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'production') {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
       navigator.serviceWorker.register(swPath).then(registration => {
         console.log('Service worker registered');
