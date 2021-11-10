4fe5b2118c1cdcf9956c75037d80e0d4aa5e110e
Zhenhua Che <zhenhua.che@shopee.com>
2021-11-10 10:45:08 +0800

    fix: 将HistoryRouter换成了HashRouter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React, { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import YueKangCode from './pages/YueKangCode';
 import About from './pages/About';
-// import { buildBaseURL } from '../configs/constants';
-const genPath = (path) => {
-  return (process.env.NODE_ENV === 'development' ? '' : process.env.PUBLIC_URL) + path;
-}
+
 const App = () => {
   return (
     <Suspense fallback={<span>loading</span>}>
       <Routes>
-        <Route path={genPath('/')} element={<YueKangCode />} />
-        <Route path={genPath('/yuekangcode')} element={<YueKangCode />} />
-        <Route path={genPath('/about')} element={<About />} />
+        <Route path='/' element={<YueKangCode />} />
+        <Route path='/yuekangcode' element={<YueKangCode />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </Suspense>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +8,9 @@ import LocalServiceWorkerRegister from './sw-register';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
