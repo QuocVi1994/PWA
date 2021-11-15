32d7a5a75d8dbbd7a8c62525490799a7fa49f2ef
Aazk <532356408@qq.com>
2021-11-15 22:24:22 +0800

    feat: 完成底部动画

--- src/pages/YueKangeCode/index.tsx
+++ src/pages/YueKangeCode/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import './index.css'
 
-const YueKangCode: React.FC = () => <div>123</div>
+const YueKangCode: React.FC = () => {
+  const colArr = new Array(20).fill(1)
+  const rowArr = new Array(10).fill(1)
+  return (
+    <div className="container">
+      {/* <img className="img" src="../../../assests/imgs/yuekangma.jpg" /> */}
+      <div className="bgContainer">
+        {
+          colArr.map(() => <div className="bgLineContainer">
+            {
+              rowArr.map(() => <img className="bgImg" src="../../../assests/imgs/bg.png" />)
+            }
+          </div>)
+        }
+      </div>
+    </div>
+  )
+}
 
 export default YueKangCode;
