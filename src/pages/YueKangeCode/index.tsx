85324755fcf4b8352c32fd1a0d5642a487d4804c
Zhenhua Che <zhenhua.che@shopee.com>
2021-11-16 19:07:33 +0800

    feat: 完成了输入框的信息

--- src/pages/YueKangeCode/index.tsx
+++ src/pages/YueKangeCode/index.tsx
@@ -1,4 +1,4 @@
-import React,{ useMemo } from 'react';
+import React,{ useCallback, useState, useEffect } from 'react';
 import './index.css'
 import bgIcon from '../../../assests/imgs/bg.png'
 import infoIcon from '../../../assests/imgs/info_702_788.png'
@@ -9,6 +9,43 @@ const colArr = new Array(Math.ceil(scrollBgHeight / 40) + 10).fill(1);
 const rowArr = new Array(Math.ceil(windowWidth / 80)).fill(1);
 
 const YueKangCode: React.FC = () => {
+
+  const [timestamp, setTimestamp] = useState('')
+  const [name, setName] = useState(localStorage.getItem('name') || '某*某');
+  const [address, setAddress] = useState(localStorage.getItem('address') || '广州');
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date()
+      const month = `${now.getMonth() + 1}`.padStart(2, '0')
+      const day = `${now.getDate()}`.padStart(2, '0');
+      const hour = `${now.getHours()}`.padStart(2, '0');
+      const min = `${now.getMinutes()}`.padStart(2, '0');
+      const sec = `${now.getSeconds()}`.padStart(2, '0');
+      setTimestamp(`${month}-${day} ${hour}:${min}:${sec}`)
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [])
+
+  const popupAddress = useCallback(() => {
+    const address = prompt("请输入地址", "");
+    if (address && address !== ""){
+      setAddress(address)
+      localStorage.setItem("address", address)
+    }
+  }, []);
+
+  const popupName = useCallback(() => {
+    const name = prompt("请输入名字", "");
+    if (name && name !== ""){
+      const handleName = name.length > 2 ? `${name[0]}*${name[name.length - 1]}` : `${name[0]}*`
+      setName(handleName)
+      localStorage.setItem("name", handleName)
+    }
+  }, []);
+
   return (
     <div className="container">
       {/* <img className="img" src="../../../assests/imgs/yuekangma.jpg" /> */}
@@ -22,6 +59,9 @@ const YueKangCode: React.FC = () => {
         }
       </div>
       <img className="infoContainer" src={infoIcon} />
+      <div onClick={popupAddress} className="address">{address}</div>
+      <div onClick={popupName} className="name">{name}</div>
+      <div className="time">{timestamp}</div>
     </div>
   )
 }
