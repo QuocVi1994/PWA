84ff8052cca139f0dab81049f79e4cc83004c6b3
Zhenhua Che <zhenhua.che@shopee.com>
2021-11-17 14:42:56 +0800

    feat: 初步完成

--- src/pages/YueKangeCode/index.tsx
+++ src/pages/YueKangeCode/index.tsx
@@ -1,7 +1,9 @@
-import React,{ useCallback, useState, useEffect } from 'react';
-import './index.css'
-import bgIcon from '../../../assests/imgs/bg.png'
-import infoIcon from '../../../assests/imgs/info_702_788.png'
+import React, { useCallback, useState, useEffect } from "react";
+import "./index.css";
+import bgIcon from "../../../assests/imgs/bg.png";
+import infoIcon from "../../../assests/imgs/info_702_788.png";
+import otherInfoIcon from "../../../assests/imgs/other_info_704_252.png";
+import fullImageBot from "../../../assests/imgs/yuekangma_bot.jpeg";
 
 const windowWidth = document.body.clientWidth;
 const scrollBgHeight = windowWidth * (683 / 375);
@@ -9,61 +11,89 @@ const colArr = new Array(Math.ceil(scrollBgHeight / 40) + 10).fill(1);
 const rowArr = new Array(Math.ceil(windowWidth / 80)).fill(1);
 
 const YueKangCode: React.FC = () => {
-
-  const [timestamp, setTimestamp] = useState('')
-  const [name, setName] = useState(localStorage.getItem('name') || '某*某');
-  const [address, setAddress] = useState(localStorage.getItem('address') || '广州');
+  const [timestamp, setTimestamp] = useState("");
+  const [name, setName] = useState(localStorage.getItem("name") || "某*某");
+  const [address, setAddress] = useState(
+    localStorage.getItem("address") || "广州"
+  );
 
   useEffect(() => {
     const timer = setInterval(() => {
-      const now = new Date()
-      const month = `${now.getMonth() + 1}`.padStart(2, '0')
-      const day = `${now.getDate()}`.padStart(2, '0');
-      const hour = `${now.getHours()}`.padStart(2, '0');
-      const min = `${now.getMinutes()}`.padStart(2, '0');
-      const sec = `${now.getSeconds()}`.padStart(2, '0');
-      setTimestamp(`${month}-${day} ${hour}:${min}:${sec}`)
+      const now = new Date();
+      const month = `${now.getMonth() + 1}`.padStart(2, "0");
+      const day = `${now.getDate()}`.padStart(2, "0");
+      const hour = `${now.getHours()}`.padStart(2, "0");
+      const min = `${now.getMinutes()}`.padStart(2, "0");
+      const sec = `${now.getSeconds()}`.padStart(2, "0");
+      setTimestamp(`${month}-${day} ${hour}:${min}:${sec}`);
     }, 1000);
     return () => {
       clearInterval(timer);
     };
-  }, [])
+  }, []);
 
   const popupAddress = useCallback(() => {
     const address = prompt("请输入地址", "");
-    if (address && address !== ""){
-      setAddress(address)
-      localStorage.setItem("address", address)
+    if (address && address !== "") {
+      setAddress(address);
+      localStorage.setItem("address", address);
     }
   }, []);
 
   const popupName = useCallback(() => {
     const name = prompt("请输入名字", "");
-    if (name && name !== ""){
-      const handleName = name.length > 2 ? `${name[0]}*${name[name.length - 1]}` : `${name[0]}*`
-      setName(handleName)
-      localStorage.setItem("name", handleName)
+    if (name && name !== "") {
+      const handleName =
+        name.length > 2 ? `${name[0]}*${name[name.length - 1]}` : `${name[0]}*`;
+      setName(handleName);
+      localStorage.setItem("name", handleName);
     }
   }, []);
 
   return (
-    <div className="container">
-      {/* <img className="img" src="../../../assests/imgs/yuekangma.jpg" /> */}
-      <div className="bgContainer">
-        {
-          colArr.map((_, index) => <div key={`line_${index}`} className="bgLineContainer">
-            {
-              rowArr.map((_, index2) => <img key={`img_${index2}`} className="bgImg" src={bgIcon} />)
-            }
-          </div>)
-        }
+    <div>
+      <div className="container">
+        <div className="bgContainer">
+          {colArr.map((_, index) => (
+            <div key={`line_${index}`} className="bgLineContainer">
+              {rowArr.map((_, index2) => (
+                <img key={`img_${index2}`} className="bgImg" src={bgIcon} />
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="titleBar">
+          <div className="leftArrow" />
+          <div className="title">粤康码</div>
+          <div className="rightIcon">
+            <div className="left">
+              <div className="smallCircle circle" />
+              <div className="bigCircle circle" />
+              <div className="smallCircle circle" />
+            </div>
+            <div className="middle" />
+            <div className="circle right">
+              <div className="middleCircle circle" />
+            </div>
+          </div>
+        </div>
+        <img className="infoContainer" src={infoIcon} />
+        <img className="otherInfoContainer" src={otherInfoIcon} />
+        <div onClick={popupAddress} className="address">
+          {address}
+        </div>
+        <div onClick={popupName} className="name">
+          {name}
+        </div>
+        <div className="time">{timestamp}</div>
+        <div className="text">
+          <div>依托全国一体化政务服务平台</div>
+          <div>实现跨省（区、市）数据共享和互通互认</div>
+        </div>
       </div>
-      <img className="infoContainer" src={infoIcon} />
-      <div onClick={popupAddress} className="address">{address}</div>
-      <div onClick={popupName} className="name">{name}</div>
-      <div className="time">{timestamp}</div>
+      <img className="img" src={fullImageBot} />
     </div>
-  )
-}
+  );
+};
 
 export default YueKangCode;
